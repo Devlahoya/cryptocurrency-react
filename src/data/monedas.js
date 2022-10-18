2f92b410f5697c7e93050441042b2a7354cd359f
Devlahoya <devlahoya@gmail.com>
2022-10-18 00:12:22 -0500

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    { id: 'USD', nombre: 'United States Dolar'},
+    { id: 'MXN', nombre: 'Peso Mexicano'},
+    { id: 'EUR', nombre: 'Euro'},
+    { id: 'GBP', nombre: 'Pound'},
+]
+
+export {
+    monedas
+}
